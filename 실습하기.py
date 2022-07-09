601769f5b57219e434025fca833502cbb8c50c26
seungbeen <seungbin803@gmail.com>
2022-07-09 14:49:00 +0900

    63p, 64p 실습하기

---
+++ 실습하기.py
@@ -0,0 +1,45 @@
+print("1")
+student_grade = "2학년"
+student_class = 4
+student_number = 11
+student_name = "최승빈"
+student_height = 163.7
+print(student_grade)
+print(student_class)
+print(student_number)
+print(student_name)
+print(student_height)
+
+print()
+print("2")
+print(type(student_grade))
+print(type(student_class))
+print(type(student_number))
+print(type(student_name))
+print(type(student_height))
+
+print()
+print("3")
+our_team = ["김민영", "김예진", "박예진", "신유림", "양서영", "오나현", "유정은", "윤성현", "임수민", "최가을", "최승빈", "최원서", "황채민"]
+print(our_team)
+
+print()
+print("4")
+print(our_team[9])
+
+print()
+print("5")
+print(our_team[5:9])
+
+print()
+print("6")
+club = {"김민영":"앱앤미", "김예진":"LSC", "박예진":"JS", "신유림":"JS", "양서영":"없음", "오나현":"LSC", "유정은":"PP", "윤성현":"없음", "임수민":"MAS", "최가을":"CPU", "최승빈":"앱앤미", "최원서":"LSC", "황채민":"CPU"}
+print(club)
+
+print()
+print("7")
+print(club["오나현"])
+
+print()
+print("8")
+print(club[our_team[0]])
